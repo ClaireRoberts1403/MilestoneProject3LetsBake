01e6e0173f5cc88d4a15832fed335c5b3d337285
ClaireRoberts1403 <55883949+ClaireRoberts1403@users.noreply.github.com>
2020-09-09 23:26:54 +0100

    edit recipe was unsuccessful some sight changes to stylings

--- app.py
+++ app.py
@@ -15,18 +15,19 @@ app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 
 mongo = PyMongo(app)
 
+#home page
 @app.route('/')
 def index():
     recipe_of_the_week = mongo.db.Recipes.find()
     return render_template("index.html", Recipes=recipe_of_the_week)
 
-# send user to add recipe page form
+# send user to add recipe form
 @app.route('/add_recipe')
 def add_recipe():
     return render_template("add_recipe.html")
 
 
-# submit recipe page the RUD
+# submit recipe page
 @app.route('/submit_recipe', methods=["POST"])
 def submit_recipe():
     add_recipe = mongo.db.Recipes
@@ -58,7 +59,7 @@ def submit_recipe():
     mongo.save_file(recipe_image.filename, recipe_image)
     return render_template("Message.html")
 
-
+# To display full recipe with link to edit and delete recipe
 @app.route('/recipe')
 def recipe():
     Recipes = mongo.db.Recipes.find_one()
@@ -74,6 +75,19 @@ def image(recipe_image):
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template("page_not_found.html"), 404
+
+# edit function
+#@app.route('/edit_recipe/<recipe_id>')
+#def edit_recipe(recipe_id):
+#    the_recipe = mongo.db.Recipes.find_one({"_id": ObjectId(recipe_id)})
+#    all_recipes = mongo.db.Recipes.find()
+#    return render_template(edit_recipe.html, recipe=the_recipe, Recipes=all_recipes)
+
+# delete function
+#@app.route('/recipe/<Recipes_id>/delete')
+#def deleteRecipe(Recipes_id):
+#    mongo.db.Recipes.remove({'_id': ObjectId(Recipes_id)})
+#    return render_template("index.html")
 
 
 if __name__ == '__main__':
