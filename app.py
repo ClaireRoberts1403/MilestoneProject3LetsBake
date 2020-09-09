c0f43b0e8f7f98ab5aa3e55aead14b52de569298
ClaireRoberts1403 <55883949+ClaireRoberts1403@users.noreply.github.com>
2020-09-09 21:34:30 +0100

    add recipe succesfully adds recipe and displays thank you message on return recipe is displayed on home screen

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
-from flask import Flask, render_template
-from flask_pymongo import PyMongo
+from flask import Flask, render_template, request
+from flask_pymongo import PyMongo, pymongo
+from bson.objectid import ObjectId
 
 if os.path.exists("env.py"):
     import env
@@ -24,6 +25,43 @@ def index():
 def add_recipe():
     return render_template("add_recipe.html")
 
+
+# submit recipe page the RUD
+@app.route('/submit_recipe', methods=["POST"])
+def submit_recipe():
+    add_recipe = mongo.db.Recipes
+
+    name = request.form['name']
+    category_name = request.form['category_name']
+    prep_time = request.form['prep_time']
+    cooking_time = request.form['cooking_time']
+    effort_level = request.form['effort_level']
+    serves = request.form['serves']
+    ingredients = request.form['ingredients']
+    method = request.form['method']
+    recipe_image = request.files['recipe_image']
+
+    add_recipe_form = {
+        "name": name,
+        "category_name": category_name,
+        "prep_time": prep_time,
+        "cooking_time": cooking_time,
+        "effort_level": effort_level,
+        "serves": serves,
+        "ingredients": ingredients,
+        "method": method,
+        "recipe_image": recipe_image,
+
+    }
+    add_recipe.insert_one(request.form.to_dict())
+    mongo.save_file(recipe_image.filename, recipe_image)
+    return render_template("Message.html")
+
+
+#test to see recipe image returning from mongodb
+@app.route('/image/<recipe_image>')
+def image(recipe_image):
+    return mongo.send_file(recipe_image)
 
 
 if __name__ == '__main__':
